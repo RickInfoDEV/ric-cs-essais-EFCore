7dfcde3ce49ac1bad859b287187dd6661cf11553
Richard PAYET <rick.info.rsoc@gmail.com>
2022-04-19 14:04:28 +0200

    1er commit : Solution  Web ASP.NET Core + Nugets EF Core 5.0.16 +  Paramétrage minimal DbContext / appsettings.json.

---
+++ WizLib/WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WizLib_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
